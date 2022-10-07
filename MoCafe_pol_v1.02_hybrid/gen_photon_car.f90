b90cab257d87971c58e46561a10c6702308f3b72
Kwang-il Seon <kiseon@kasi.re.kr>
2022-10-07 16:59:34 +0900

    .

--- MoCafe_pol_v1.02_hybrid/gen_photon_car.f90
+++ MoCafe_pol_v1.02_hybrid/gen_photon_car.f90
@@ -13,8 +13,6 @@ contains
   ! local variables
   real(kind=wp) :: sint,cost,phi,sinp,cosp,rp
   real(kind=wp) :: kx,ky,kz,mx,my,mz,nx,ny,nz
-  real(kind=wp) :: delt(6), dist
-  integer       :: loc(1)
 
   !=== set up photon's position vector.
   if (trim(par%source_geometry) == 'uniform' .and. par%rmax > 0.0_wp) then
@@ -41,7 +39,7 @@ contains
      photon%x = grid%xrange*rand_number()+grid%xmin
      photon%y = grid%yrange*rand_number()+grid%ymin
      photon%z = par%source_zscale*rand_zexp(par%zmax/par%source_zscale)
-  else if (trim(par%source_geometry(1:8)) == 'external') then
+  else if (trim(par%source_geometry(1:12)) == 'external_sph') then
      !--- random number for incident location
      cost = 2.0_wp*rand_number() - 1.0_wp
      sint = sqrt(1.0_wp-cost*cost)
@@ -61,47 +59,112 @@ contains
      nz =  0.0_wp
 
      !--- position of the incident photon
-     if (par%rmax > 0.0_wp) then
-        photon%x = par%rmax*kx
-        photon%y = par%rmax*ky
-        photon%z = par%rmax*kz
-     else
-        delt(1) =  par%xmax/kx
-        delt(2) = -par%xmax/kx
-        delt(3) =  par%ymax/ky
-        delt(4) = -par%ymax/ky
-        delt(5) =  par%zmax/kz
-        delt(6) = -par%zmax/kz
-        loc     = minloc(delt, delt >= 0.0_wp)
-        dist    = delt(loc(1))
-        photon%x = dist*kx
-        photon%y = dist*ky
-        photon%z = dist*kz
-        select case(loc(1))
-           case (1)
-              photon%x = grid%xface(grid%nx+1)
-           case (2)
-              photon%x = grid%xface(1)
-           case (3)
-              photon%y = grid%yface(grid%ny+1)
-           case (4)
-              photon%y = grid%yface(1)
-           case (5)
-              photon%z = grid%zface(grid%nz+1)
-           case (6)
-              photon%z = grid%zface(1)
-        end select
-     endif
+     !--- please note that par%rmax should be positive
+     photon%x = par%rmax*kx
+     photon%y = par%rmax*ky
+     photon%z = par%rmax*kz
 
      !--- random direction for propagation direction
      !--- note that pi/2 < theta < pi, i.e., -1 < cost < 0.
-     !--- bug-fixed, 2021.11.01
-     !cost = -rand_number()
      cost = -sqrt(rand_number())
      sint = sqrt(1.0_wp-cost*cost)
      phi  = twopi*rand_number()
      cosp = cos(phi)
      sinp = sin(phi)
+
+     photon%kx = sint * (cosp * mx + sinp * nx) + cost * kx
+     photon%ky = sint * (cosp * my + sinp * ny) + cost * ky
+     photon%kz = sint * (cosp * mz + sinp * nz) + cost * kz
+  else if (trim(par%source_geometry(1:12)) == 'external_rec') then
+     !--- random direction for propagation direction
+     !--- note that pi/2 < theta < pi, i.e., -1 < cost < 0.
+     cost = -sqrt(rand_number())
+     sint = sqrt(1.0_wp-cost*cost)
+     phi  = twopi*rand_number()
+     cosp = cos(phi)
+     sinp = sin(phi)
+
+     !--- random number for incident location
+     select case(floor(6.0*rand_number() + 1.0))
+     case (1)
+        photon%x = grid%xface(grid%nx+1)
+        photon%y = grid%yrange * rand_number() + grid%ymin
+        photon%z = grid%zrange * rand_number() + grid%zmin
+        kx = 1.0
+        ky = 0.0
+        kz = 0.0
+        mx = 0.0
+        my = 1.0
+        mz = 0.0
+        nx = 0.0
+        ny = 0.0
+        nz = 1.0
+     case (2)
+        photon%x = grid%xface(1)
+        photon%y = grid%yrange * rand_number() + grid%ymin
+        photon%z = grid%zrange * rand_number() + grid%zmin
+        kx = -1.0
+        ky = 0.0
+        kz = 0.0
+        mx = 0.0
+        my = 1.0
+        mz = 0.0
+        nx = 0.0
+        ny = 0.0
+        nz = 1.0
+     case (3)
+        photon%y = grid%yface(grid%ny+1)
+        photon%z = grid%zrange * rand_number() + grid%zmin
+        photon%x = grid%xrange * rand_number() + grid%xmin
+        kx = 0.0
+        ky = 1.0
+        kz = 0.0
+        mx = 1.0
+        my = 0.0
+        mz = 0.0
+        nx = 0.0
+        ny = 0.0
+        nz = 1.0
+     case (4)
+        photon%y = grid%yface(1)
+        photon%z = grid%zrange * rand_number() + grid%zmin
+        photon%x = grid%xrange * rand_number() + grid%xmin
+        kx = 0.0
+        ky = -1.0
+        kz = 0.0
+        mx = 1.0
+        my = 0.0
+        mz = 0.0
+        nx = 0.0
+        ny = 0.0
+        nz = 1.0
+     case (5)
+        photon%z = grid%zface(grid%nz+1)
+        photon%x = grid%xrange * rand_number() + grid%xmin
+        photon%y = grid%yrange * rand_number() + grid%ymin
+        kx = 0.0
+        ky = 0.0
+        kz = 1.0
+        mx = 1.0
+        my = 0.0
+        mz = 0.0
+        nx = 0.0
+        ny = 1.0
+        nz = 0.0
+     case (6)
+        photon%z = grid%zface(1)
+        photon%x = grid%xrange * rand_number() + grid%xmin
+        photon%y = grid%yrange * rand_number() + grid%ymin
+        kx = 0.0
+        ky = 0.0
+        kz = -1.0
+        mx = 1.0
+        my = 0.0
+        mz = 0.0
+        nx = 0.0
+        ny = 1.0
+        nz = 0.0
+     end select
 
      photon%kx = sint * (cosp * mx + sinp * nx) + cost * kx
      photon%ky = sint * (cosp * my + sinp * ny) + cost * ky
